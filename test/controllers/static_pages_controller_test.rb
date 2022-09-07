0b2ad101652fd26dcaa837e7bf3ddd36f0b7bce7
mangusidan <manhnv130100@gmail.com>
2022-09-07 14:54:08 +0700

    Static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "E-Learning System"
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 end
