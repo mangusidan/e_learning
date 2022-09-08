8d82456155eb82d3d862526236f86735fda9bc8c
mangusidan <manhnv130100@gmail.com>
2022-09-08 16:41:44 +0700

    ...create layout
    init model
    
    created layout
    
    create_layout
    
    create layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "sessions/new"
   get "users/new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
   root "static_pages#home"
   get "static_pages/home"
+  get "/signup", to: "users#new"
 end
