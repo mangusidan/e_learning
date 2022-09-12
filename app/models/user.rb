b598f13a07a7c845b0058da5cc41e61c08cea7a4
mangusidan <manhnv130100@gmail.com>
2022-09-12 12:00:04 +0700

    Login-logout
    login logout
    
    Login-logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_many :activities
   has_many :active_relationships,  class_name: Relationship.name,
                                    foreign_key: :follower_id, dependent: :destroy
@@ -15,6 +16,32 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX} 
   validates :password, presence: true, length: {minimum: 6} , allow_nil: true
   has_secure_password
+  
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
   
   private
     def downcase_email
