96d0e97e4ecc85cc4e4f6fd1659391d36a9c36ba
mangusidan <manhnv130100@gmail.com>
2022-09-12 18:15:29 +0700

    show user, edit user profile
    show user, edit user profile
    
    show user,edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get "sessions/new"
-  get "users/new"
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  root "static_pages#home"
-  get "static_pages/home"
-  get "/signup", to: "users#new"
-  delete "/logout", to: "sessions#destroy"
+  root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
